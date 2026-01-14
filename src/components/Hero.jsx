8f6821f9fafb4faebe0801a03def002cd6835a1d
N1nji <pedro.felipe7911@gmail.com>
2026-01-13 22:38:14 -0300

    Photo temp 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,7 +14,7 @@ export default function Hero() {
         data-aos="zoom-in"
       >
         <img
-          src="assets/team/photo temp.jpeg"
+          src="assets/team/photo temp2.jpeg"
           className="w-full h-full object-cover rounded-full border-4 border-indigo-400 shadow-md select-none pointer-events-none"
         />
       </div>
